2c877a30cf8e1ba490950072f0e8919cc1751c8f
Otavio Gomes <otaviogomes@protonmail.com>
2022-10-09 12:22:34 -0300

    fix invalid number rule

--- freebsd_firewall.sh
+++ freebsd_firewall.sh
@@ -169,7 +169,7 @@ ipfw -q add 04999 deny log all from any to any
 ## Outbound IPv4 NAT
 ipfw -q 9999 add nat 1 ip4 from any to any out xmit ${wan_if}
 
-ipfw -q add 100001 allow ip4 from any to any
+ipfw -q add 10001 allow ip4 from any to any
 
 EOF
 
